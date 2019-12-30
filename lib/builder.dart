6dfe4b729f5ad0b2b45336b3b3ae781b8d472894
Jansid <jianliqing617@pingan.com.cn>
2019-12-30 16:33:29 +0800

    初始化项目提交

---
+++ lib/builder.dart
@@ -0,0 +1,7 @@
+library jrouter.builder;
+
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
+
+// Builder routeBuilder(BuilderOptions options) =>
+  // LibraryBuilder()
